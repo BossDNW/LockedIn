cd4836a7de20c7e6c4441aea3d0890a46acdf807
Chernique <hchernique@gmail.com>
2026-04-14 16:33:09 -0400

    Update my_applications.py

--- app/routers/my_applications.py
+++ app/routers/my_applications.py
@@ -3,12 +3,9 @@ from fastapi.responses import HTMLResponse
 from app.dependencies.session import SessionDep
 from app.dependencies.auth import AuthDep
 from app.routers import templates
-from app.models.user import Application, Programme, Company
-from sqlmodel import select
 
 router = APIRouter(tags=["My Applications"])
 
-# HTML PAGE - View my applications (ONLY for students)
 @router.get("/my-applications", response_class=HTMLResponse)
 async def my_applications_page(request: Request, user: AuthDep):
     """Render the my applications page - ONLY STUDENTS CAN ACCESS"""
