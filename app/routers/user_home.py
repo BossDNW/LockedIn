e75c9da074430e4893c82063ede8202f315f6d02
Chernique <hchernique@gmail.com>
2026-04-14 16:33:13 -0400

    Update user_home.py

--- app/routers/user_home.py
+++ app/routers/user_home.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, HTTPException, Depends, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi import status
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from app.dependencies.auth import AuthDep
 from app.dependencies.session import SessionDep
-from app.dependencies.auth import AuthDep, IsUserLoggedIn, get_current_user, is_admin
 from . import router, templates
 
 
@@ -10,7 +9,7 @@ from . import router, templates
 async def user_home_view(
     request: Request,
     user: AuthDep,
-    db:SessionDep
+    db: SessionDep
 ):
     return templates.TemplateResponse(
         request=request, 
