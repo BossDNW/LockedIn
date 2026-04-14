ef01df0248665899536a65e6515dd9fdeadbd99c
Chernique <hchernique@gmail.com>
2026-04-14 15:59:12 -0400

    Update index.py

--- app/routers/index.py
+++ app/routers/index.py
@@ -13,10 +13,14 @@ async def index_view(
 ):
     if user_logged_in:
         user = await get_current_user(request, db)
-        if await is_admin(user):
-            return RedirectResponse(url=request.url_for('admin_home_view'), status_code=status.HTTP_303_SEE_OTHER)
-        return RedirectResponse(url=request.url_for('user_home_view'), status_code=status.HTTP_303_SEE_OTHER)
-    response = RedirectResponse(url=request.url_for('login_view'), status_code=status.HTTP_303_SEE_OTHER)
+        if user.role == 'admin':
+            return RedirectResponse(url="/admin", status_code=status.HTTP_303_SEE_OTHER)
+        elif user.role == 'company':
+            return RedirectResponse(url="/candidates", status_code=status.HTTP_303_SEE_OTHER)
+        else:  # student
+            return RedirectResponse(url="/my-applications", status_code=status.HTTP_303_SEE_OTHER)
+    
+    response = RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
     response.delete_cookie(
         key="access_token", 
         httponly=True,
